cba117beec1df07baa890a9d5bbc8fb65cc4b38b
mclegacycodes <kingsleytwum2@gmail.com>
2023-04-08 20:59:30 +0000

    finished creating the how to apply section

--- src/components/UI/HowToApply.js
+++ src/components/UI/HowToApply.js
@@ -7,13 +7,13 @@ import classes from "./HowToApply.module.css";
 const HowToApply = () => {
   return (
     <div>
-        <NeutralBg>
+        <NeutralBg className={classes.col}>
             <HeadingText3 title="How to Apply"/>
             <div className={classes.stepsWrapper}>
                <StepCard number="1" action="Visit apply-stufso.ucc.edu.gh"/>
-               <StepCard number="1" action="Visit apply-stufso.ucc.edu.gh"/>
-               <StepCard number="1" action="Visit apply-stufso.ucc.edu.gh"/>
-               <StepCard number="1" action="Visit apply-stufso.ucc.edu.gh"/>
+               <StepCard number="2" action="Create account using the Sign Up button"/>
+               <StepCard number="3" action="Complete the application form and upload the necessary documents."/>
+               <StepCard number="4" action="Short listed application will be invited to a selection interview. the University"/>
             </div>
         </NeutralBg>
     </div>
--- src/components/UI/StepCard.js
+++ src/components/UI/StepCard.js
@@ -4,7 +4,7 @@ import classes from "./StepCard.module.css";
 
 const StepCard = (props) => {
   return (
-    <div className={classes.card}>
+    <div >
         <ExcerptCard className={classes.card}>
             <span className={classes.number}>{props.number}</span>
              <p className={classes.action}>{props.action}</p>
