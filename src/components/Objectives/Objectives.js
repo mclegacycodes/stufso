8e70ff22d0b8f59df124829d6a14691990a537cb
mclegacycodes <kingsleytwum2@gmail.com>
2023-04-08 12:08:36 +0000

    fixed the white background for the objectives section

--- src/components/Objectives/Objectives.js
+++ src/components/Objectives/Objectives.js
@@ -9,16 +9,16 @@ const Objectives = () => {
     <div>
         <SectionTemplate title="Our Objectives" bgImage={sampleImg} bgColor={classes.blue}>
             <div className={classes.listWrapper}>
-                <Eligibility__ListItem number="01" content="Provide financial support in the form of bursaries and
+                <Eligibility__ListItem contentBg={classes.white} number="01" content="Provide financial support in the form of bursaries and
              scholarships to Needy but brilliant students of the University of Cape Coast."/>
 
-             <Eligibility__ListItem number="02" content="Provide financial support in the form of bursaries and
+             <Eligibility__ListItem contentBg={classes.white} number="02" content="Provide financial support in the form of bursaries and
              scholarships to Needy but brilliant students of the University of Cape Coast."/>
 
-             <Eligibility__ListItem number="03" content="Provide financial support in the form of bursaries and
+             <Eligibility__ListItem contentBg={classes.white} number="03" content="Provide financial support in the form of bursaries and
              scholarships to Needy but brilliant students of the University of Cape Coast."/>
 
-             <Eligibility__ListItem number="04" content="Provide financial support in the form of bursaries and
+             <Eligibility__ListItem contentBg={classes.white} number="04" content="Provide financial support in the form of bursaries and
              scholarships to Needy but brilliant students of the University of Cape Coast."/>
             </div>
             
